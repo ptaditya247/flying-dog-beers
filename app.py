5dd2c1413e8e04b8f465218dc1f18b863fea4fb1
ptaditya247 <53501835+ptaditya247@users.noreply.github.com>
2019-10-09 17:33:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import locale
 
-locale.setlocale(locale.LC_ALL)
+locale.setlocale(locale.LC_ALL,'EN_US')
 
 
 ########### Set up the chart
